b9625a113133b2047452f3efddb2b17286d086a0
christoffer <cno_leet@pm.me>
2026-05-02 18:09:36 +0200

    added text node to html node + tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -19,6 +19,16 @@ class TestTextNode(unittest.TestCase):
         node5 = TextNode("This is another text node", TextType.ITALIC, "https://google.com")
         node6 = TextNode("This is another text node", TextType.CODE)
         self.assertNotEqual(node5, node6)
+
+    def test_text(self):
+        text_node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(text_node)
+        expected_output = 'This is a text node'
+        print("\nTesting text_node_to_html_node() on html_node1..")
+        print(f"Expected result: {expected_output}")
+        print(f"Actual:          {html_node}")
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, expected_output)
 
 if __name__ == "__main__":
     unittest.main()
